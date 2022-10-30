a3704b1343fe203e2eb86d3f973351afb4d32d78
Mohamad Supangat <hantamkoding@gmail.com>
2022-10-30 21:48:11 +0700

    update: autofill form js

--- greasemonkey/Auto Form Filler.user.js
+++ greasemonkey/Auto Form Filler.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Auto Fill Form Data
 // @description    Auto Fill FOrm Data
-// @require        https://cdn.jsdelivr.net/gh/mohamad-supangat/form-autofill-js@1.0.1/dist/main.js
+// @require        https://cdn.jsdelivr.net/gh/mohamad-supangat/form-autofill-js@1.0.2/dist/main.js
 // ==/UserScript==
 
 Autofill.tool();
